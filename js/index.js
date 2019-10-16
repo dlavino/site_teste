a5b55c8d071358509159de9f6041976db3503c51
dlavino <31008349+dlavino@users.noreply.github.com>
2019-10-16 11:56:23 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,239 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+
+var select = tail.select("select", {
+    locale:"pt_BR",
+    search:true,
+    multiSelectAll:true,
+    multiShowCount:false,
+    multiContainer:true,
+    width:"100%",
+    classNames:true
+});
+
+
+var chartData = {
+  labels: ["1", "2", "3", "4", "5", "6", "7"],
+  datasets: [{
+    data: [589, 445, 483, 503, 689, 692, 634],
+    backgroundColor: 'transparent',
+    borderColor: '#000',
+    pointBackgroundColor: '#000',
+  }]
+};
+
+var chLine = document.getElementById("myChart");
+if (chLine) {
+  new Chart(chLine, {
+  type: 'line',
+  data: chartData,
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
+    elements: {
+        line: {
+            tension: 0 // disables bezier curves
+        }
+    }
+  }
+  });
+}
+
+
+
+
+
+
+
+
+var $table = $('#table')
+var $remove = $('#remove')
+var selections = []
+
+function getIdSelections() {
+  return $.map($table.bootstrapTable('getSelections'), function (row) {
+    return row.id
+  })
+}
+
+function responseHandler(res) {
+  $.each(res.rows, function (i, row) {
+    row.state = $.inArray(row.id, selections) !== -1
+  })
+  return res
+}
+
+function detailFormatter(index, row) {
+  var html = []
+  $.each(row, function (key, value) {
+    html.push('<p><b>' + key + ':</b> ' + value + '</p>')
+  })
+  return html.join('')
+}
+
+function operateFormatter(value, row, index) {
+  return [
+    '<a class="like" href="javascript:void(0)" title="Like">',
+    '<i class="fa fa-heart"></i>',
+    '</a>  ',
+    '<a class="remove" href="javascript:void(0)" title="Remove">',
+    '<i class="fa fa-trash"></i>',
+    '</a>'
+  ].join('')
+}
+
+window.operateEvents = {
+  'click .like': function (e, value, row, index) {
+    alert('You click like action, row: ' + JSON.stringify(row))
+  },
+  'click .remove': function (e, value, row, index) {
+    $table.bootstrapTable('remove', {
+      field: 'id',
+      values: [row.id]
+    })
+  }
+}
+
+function totalTextFormatter(data) {
+  return 'Total'
+}
+
+function totalNameFormatter(data) {
+  return data.length
+}
+
+function totalPriceFormatter(data) {
+  var field = this.field
+  return '$' + data.map(function (row) {
+    return +row[field].substring(1)
+  }).reduce(function (sum, i) {
+    return sum + i
+  }, 0)
+}
+
+function initTable() {
+  $table.bootstrapTable('destroy').bootstrapTable({
+    height: 550,
+    locale: $('#locale').val(),
+    columns: [
+      [{
+        field: 'state',
+        checkbox: true,
+        rowspan: 2,
+        align: 'center',
+        valign: 'middle'
+      }, {
+        title: 'Item ID',
+        field: 'id',
+        rowspan: 2,
+        align: 'center',
+        valign: 'middle',
+        sortable: true,
+        footerFormatter: totalTextFormatter
+      }, {
+        title: 'Item Detail',
+        colspan: 3,
+        align: 'center'
+      }],
+      [{
+        field: 'name',
+        title: 'Item Name',
+        sortable: true,
+        footerFormatter: totalNameFormatter,
+        align: 'center'
+      }, {
+        field: 'price',
+        title: 'Item Price',
+        sortable: true,
+        align: 'center',
+        footerFormatter: totalPriceFormatter
+      }, {
+        field: 'operate',
+        title: 'Item Operate',
+        align: 'center',
+        clickToSelect: false,
+        events: window.operateEvents,
+        formatter: operateFormatter
+      }]
+    ]
+  })
+  $table.on('check.bs.table uncheck.bs.table ' +
+    'check-all.bs.table uncheck-all.bs.table',
+  function () {
+    $remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
+
+    // save your data, here just save the current page
+    selections = getIdSelections()
+    // push or splice the selections if you want to save all data selections
+  })
+  $table.on('all.bs.table', function (e, name, args) {
+    console.log(name, args)
+  })
+  $remove.click(function () {
+    var ids = getIdSelections()
+    $table.bootstrapTable('remove', {
+      field: 'id',
+      values: ids
+    })
+    $remove.prop('disabled', true)
+  })
+}
+
+$(function() {
+  initTable()
+
+  $('#locale').change(initTable)
+})
