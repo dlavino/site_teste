ebf8ae4959f6342edf8adfbcc850e478aa85ddaa
Daniel Lavino <daniel_lavino@hotmail.com>
2021-05-19 20:26:38 -0300

    meu

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/*
+/* eu kkkkk
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+ teste teste
+ 
 var app = {
     // Application Constructor
     initialize: function() {
